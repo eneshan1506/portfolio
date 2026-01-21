996d33289e7dd8a85313d3f1b50cceff7218a5b8
eneshan1506 <eneshan0646@gmail.com>
2026-01-21 23:40:56 +0100

    add weather app

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,38 +3,43 @@ import "../css/Projects.css";
 function Projects() {
   const projects = [
     {
-  title: "To Do App",
-  desc: "Task management app with filters and localStorage",
-  github: "https://github.com/eneshan1506/todo-app",
-  live: "https://todo-app-three-liard-77.vercel.app/"
-},
-    { title: "Weather App", desc: "Weather API app" },
+      title: "To Do App",
+      desc: "Task management app with filters and localStorage",
+      github: "https://github.com/eneshan1506/todo-app",
+      live: "https://todo-app-three-liard-77.vercel.app/",
+    },
+    {
+      title: "Weather App",
+      desc: "Search city weather + 5-day forecast (Open-Meteo)",
+      github: "https://github.com/eneshan1506/weather-app",
+      live: "https://weather-app-topaz-ten-24.vercel.app/",
+    },
     { title: "Calculator", desc: "Basic calculator" },
     { title: "Movie Search", desc: "Search movies app" },
   ];
 
   return (
     <section className="projects" id="projects">
-  <h2>Projects</h2>
+      <h2>Projects</h2>
 
-  <div className="projects-container">
-    {projects.map((project, index) => (
-      <div className="project-card" key={index}>
-        <h3>{project.title}</h3>
-        <p>{project.desc}</p>
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <div className="project-card" key={index}>
+            <h3>{project.title}</h3>
+            <p>{project.desc}</p>
 
-        <div style={{ display: "flex", gap: "14px" }}>
-          <a href={project.github} target="_blank" rel="noreferrer">
-            GitHub
-          </a>
-          <a href={project.live} target="_blank" rel="noreferrer">
-            Live
-          </a>
-        </div>
+            <div style={{ display: "flex", gap: "14px" }}>
+              <a href={project.github} target="_blank" rel="noreferrer">
+                GitHub
+              </a>
+              <a href={project.live} target="_blank" rel="noreferrer">
+                Live
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-</section>
+    </section>
   );
 }
 
